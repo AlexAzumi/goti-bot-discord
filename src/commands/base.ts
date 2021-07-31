5ee32d1dec72a9450dd9f2376c25749b34216090
Alejandro Suárez <alejandro-hdez115@outlook.com>
2021-07-31 09:40:57 -0500

    Update hello command

--- src/commands/base.ts
+++ src/commands/base.ts
@@ -2,7 +2,7 @@ import { Command } from '../interfaces/command.interface'
 
 export default [
   {
-    name: 'hola',
+    name: 'hello',
     description: 'El bot te saluda',
     execute(message) {
       message.channel.send(`Hola, ${message.author}`)
